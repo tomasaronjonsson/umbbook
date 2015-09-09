4a4ffb864d7c3dd243d823bebb9bde99917a2354
tomasaronjonsson <tomas@tyrael.is>
2015-09-09 10:44:09 +0200

    adding search partial view

---
+++ UmbBook/Models/SearchResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UmbBook.Models
+{
+    public class SearchResultModel
+    {
+
+        public string Url { get; set; }
+        public string Name { get; set; }
+    }
+}
