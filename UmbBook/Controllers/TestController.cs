4e45b4dc09ff38329f22e00ec5ccc1c4064f48f3
tomasaronjonsson <tomas@tyrael.is>
2015-09-14 10:56:24 +0200

    added some angular suff to play with

---
+++ UmbBook/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace UmbBook.Controllers
+{
+    public class TestController : UmbracoApiController
+    {
+
+        public object GetAllUsers()
+        {
+            var memberService = ApplicationContext.Services.MemberService;
+
+
+            return memberService.GetAllMembers().Select(x => new { x.Name, x.CreateDate, x.Email });
+        }
+    }
+}
