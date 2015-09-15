716c16259b2d3332dbff1e6a169f4485ec5ccd86
tomasaronjonsson <tomas@tyrael.is>
2015-09-15 08:55:55 +0200

    messing around alot, updated to mssql

---
+++ UmbBook/App_Plugins/TextboxWithCharacterCount/TextboxWithCharacterCount.js
@@ -0,0 +1,45 @@
+﻿angular.module("umbraco").controller("Scylla.TextboxWithCharacterCount", function ($scope) {
+
+    // Loads Default Max Count
+    $scope.model.maxCount = $scope.model.config.maxCount;
+
+    // Attempts to re-define Max Count by description text
+    if ($scope.model.description) {
+        var maxSetInDescription = $scope.model.description.match(/\d+/);
+        if (maxSetInDescription) {
+            var newMax = parseInt(maxSetInDescription[0]);
+            if (newMax > 0)
+                $scope.model.maxCount = $scope.model.description.match(/\d+/)[0];
+        }
+    }
+
+    // Calculate a low and medium range so we can set the CSS appropriately
+    var low = Math.ceil($scope.model.maxCount * 0.25);
+    var med = Math.ceil($scope.model.maxCount * 0.5);
+
+    // Called when changes are detected within the textbox
+    $scope.update = function () {
+        // Calculate the remaining characters
+        $scope.model.remainingCount = $scope.model.maxCount - $scope.model.value.length
+
+        // Is our maximum limit reached?
+        if ($scope.model.remainingCount <= 0) {
+            $scope.model.remainingCount = 0;
+            $scope.model.value = $scope.model.value.substr(0, $scope.model.maxCount)
+            return;
+        }
+
+        // Set the correct CSS class
+        if ($scope.model.remainingCount <= low)
+            $scope.model.className = "red";
+        else if ($scope.model.remainingCount <= med)
+            $scope.model.className = "orange";
+        else
+            $scope.model.className = "green";
+
+    }
+
+    // Run the update method at start
+    $scope.update();
+
+});
